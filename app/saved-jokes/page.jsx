091ec96d67482c61cdca67571e6b70037ae0cc94
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-03-13 19:44:58 +0600

    Add superbase

---
+++ app/saved-jokes/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SavedJokesPage = () => {
+  return (
+    <main className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-center text-white p-4">
+      <h1>Header</h1>
+    </main>
+  );
+};
+
+export default SavedJokesPage;
